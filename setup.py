18942899b7b22cf0d183bff3c410a68c166e5300
lhzhanglabtools <lhzhang90@gmail.com>
2024-12-09 16:45:40 +0800

    Add files via upload

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import Command, find_packages, setup
 
-__lib_name__ = "SMILE"
+__lib_name__ = "stSMILE"
 __lib_version__ = "1.0.0"
 __description__ = "Multiscale dissection of spatial heterogeneity by integrating multi-slice spatial and single-cell transcriptomics"
 __url__ = "https://github.com/lhzhanglabtools/SMILE"
@@ -18,7 +18,7 @@ setup(
     author = __author__,
     author_email = __author_email__,
     license = __license__,
-    packages = ['SMILE'],
+    packages = ['stSMILE'],
     install_requires = __requires__,
     zip_safe = False,
     include_package_data = True,
